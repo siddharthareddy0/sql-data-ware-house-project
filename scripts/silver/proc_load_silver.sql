85446a3d5051deb432fb9a6ef0caa51a44ead5c3
siddharthareddy0 <reddynarra143@gmail.com>
2026-08-08 16:49:09 +0530

    Change default country value from 'n/n' to 'n/a'

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -191,7 +191,7 @@ begin
 		 replace(cid,'-','') as cid,
 		 case when trim(cntry) = 'DE' then 'Germany'
 			  when trim(cntry) in ('US','USA') then 'United States'
-			  when trim(cntry) = '' or cntry is null then 'n/n'
+			  when trim(cntry) = '' or cntry is null then 'n/a'
 			  else trim(cntry)
 		 end cntry
 		from bronze.erp_loc_a101;
